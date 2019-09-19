856b2044e331b9ec11a8bd359f74fb1cb4e7c9f3
Will Howes <williameldenhowes@gmail.com>
2019-09-19 16:49:22 +0100

    refactored to remove unecessary if else statement and unecessary array

--- lib/order_words.rb
+++ lib/order_words.rb
@@ -1,13 +1,4 @@
 def order_words(words)
-  sorted_words = []
-  if words.length > 8
-    words = words.split(" ").sort_by { |s| s.scan(/\d+/).first.to_i }
-    return words.join(" ")
-  end
-
-  if words.include?('c')
-    return 'c1 b2 a3'
-  end
-  return 'a1' if words.length < 3
-  'b1 a2'
+  words = words.split(" ").sort_by { |s| s.scan(/\d+/).first.to_i }
+  return words.join(" ")
 end
